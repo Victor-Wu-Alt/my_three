8d505c597ecc080da311b0cb5ccd15b1c83aab6c
victor <wuguangwei@tsingjet.com>
2023-11-08 17:38:34 +0800

    update:鬼屋初始化

--- 15-haunted-house/src/script.js
+++ 15-haunted-house/src/script.js
@@ -167,7 +167,7 @@ scene.add(moonLight)
 // const directionCameraHelper = new THREE.CameraHelper(directLight.shadow.camera)
 // directionCameraHelper.visible = false
 // scene.add(directionCameraHelper)
-
+   
 
 const spotLight = new THREE.SpotLight(0xffffff, 0.3, 6, Math.PI * 0.1, 0.25, 1)
 spotLight.position.set(0, 2, 2)
